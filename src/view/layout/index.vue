e6594a3d4a2a75ca0ff1b7db3763a427a783116f
wlongjie <1799623289@qq.com>
2019-10-14 14:55:07 +0800

    添加网页布局相关文件

---
+++ src/view/layout/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-container" >
+  </div>
+</template>
+
+<script>
+import { Navbar, AppMain } from './components'
+
+export default {
+  name: 'layout',
+  components: {
+    Navbar,
+    AppMain
+  },
+  computed: {
+  }
+}
+</script>
